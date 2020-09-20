048d34d2a20ac5456f314fa17340dcf307ff9058
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-09-20 17:43:44 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,56 +1,50 @@
-class Board
+class Board 
+  attr_accessor :cells 
 
-  attr_accessor :cells
-
-
-  def initialize
-    @cells = Array.new(9, " ")
-  end
+  @@cells = [] 
 
   def reset!
-    self.cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-  end
+    self.cells = Array.new(9, " ")
+  end 
+
+  def initialize 
+    self.cells = Array.new(9, " ")
+  end 
 
   def display
-    puts " #{cells[0]} | #{cells[1]} | #{cells[2]} "
+    puts " #{self.cells[0]} | #{self.cells[1]} | #{self.cells[2]} "
     puts "-----------"
-    puts " #{cells[3]} | #{cells[4]} | #{cells[5]} "
+    puts " #{self.cells[3]} | #{self.cells[4]} | #{self.cells[5]} "
     puts "-----------"
-    puts " #{cells[6]} | #{cells[7]} | #{cells[8]} "
+    puts " #{self.cells[6]} | #{self.cells[7]} | #{self.cells[8]} "
   end
 
-  def position(user_input)
-    cells[user_input.to_i-1]
-  end
+  def position(user_input)  
+    self.cells[user_input.to_i-1] 
+  end 
 
+  def update(position, player)
+      self.cells[position.to_i-1] = player.token
+  end 
+  #convert to board cells' array index 
+  #set it equal to the token
 
-  def full?
-    @cells.none? {|cell| cell == " "}
-  end
+  def full? 
+    self.cells.all? {|cell| cell == "X" || cell == "O"} 
+  end 
 
   def turn_count
-   counter = 0
-   @cells.each do |cell|
-      if cell == "X" || cell == "O"
-         counter += 1
-      end
-   end
-   counter
-end
+    self.cells.count("X") + self.cells.count("O") 
+  end 
 
-def taken?(position)
-# @cells != " " && @cells!= ""
+  def taken?(position)  
+    #will return true or false for an individual position
+    self.cells[position.to_i-1] == "X" || 
+    self.cells[position.to_i-1] == "O" 
+  end 
 
-  self.cells[position.to_i-1] == "X" ||
-  self.cells[position.to_i-1] == "O"
-end
-
-def valid_move?(position)
-   !taken?(position) && position.to_i >0 && position.to_i <=9
-end
-
-def update(position,player)
-  self.cells[position.to_i-1] = player.token
-end
+  def valid_move?(position) 
+    !taken?(position) && position.to_i >0 && position.to_i <=9 
+  end 
 
 end 
